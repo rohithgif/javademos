05f6922c08d2ae24625d154a308f2d58e8cbe70b
Vissa Naganjaneya <rvissanagan@teksystems.com>
2023-02-24 09:35:00 +0530

    java uploaded

---
+++ javafirst/src/javafirst/myfirst.java
@@ -0,0 +1,9 @@
+package javafirst;
+
+public class myfirst {
+	public static void main(String []args)
+	{
+		System.out.println("Hello");
+	}
+
+}
---
+++ javafirst/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rvissanagan
+ *
+ */
+module javafirst {
+}
